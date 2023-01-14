32610fa43d84235b452433f8d8b802bd4eaf946c
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-01-14 18:57:21 +0530

    Bug fixes: displaying subjects & activity section

--- main.js
+++ main.js
@@ -16,27 +16,28 @@ function showSlides(n) {
   if (n > subjects.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = subjects.length}
   for (i = 0; i < subjects.length; i++) {
-    subjects[i].style.display = "none";  
+    subjects[i].classList.add( "d-none");  
   }
-  subjects[slideIndex-1].style.display = "block";  
-  if(n < subjects.length){
-    subjects[slideIndex].style.display = "block";
-    console.log("in")
-  }
-        
-    console.log(slideIndex-1 , slideIndex)
-    console.log(subjects[slideIndex-1], subjects[slideIndex])
+  subjects[slideIndex-1].classList.remove( "d-none");  
+  subjects[slideIndex-1].classList.add( "d-block");  
 
+  if(n < subjects.length){
+    subjects[slideIndex].classList.remove( "d-none");
+    subjects[slideIndex].classList.add( "d-block");  
+  }
 }
 
-function displayContent(evt, tabName) {
+function displayContent(event, tabName) {
     console.log(tabName)
     for (let i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+      console.log(tabcontent[i])
+      tabcontent[i].classList.add( "d-none");  
     }
     for (let i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+      // tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.querySelector(`#${tabName}`).style.display = "block";
-    evt.currentTarget.className += " active";
+    document.querySelector(`#${tabName}`).classList.remove( "d-none"); 
+    document.querySelector(`#${tabName}`).classList.add( "d-block");  
+    // evt.currentTarget.className += " active";
   }
+  displayContent(event, 'upcoming')
