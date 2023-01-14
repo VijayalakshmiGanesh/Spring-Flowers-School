1a0040e03093d9846bbc827342e03ab169b16bf7
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-01-14 18:15:34 +0530

    Added functionality to Activity tabs

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-var prevButton = document.querySelector(".prev")
-var nextButton = document.querySelector(".next")
-var slides = document.querySelectorAll(".study-subjects");
-var slideIndex = 1;
 
-// showSlides(slideIndex);
+var subjects = document.querySelectorAll(".study-subjects");
+
+var tabcontent = document.querySelectorAll(".tabcontent");
+var tablinks = document.querySelectorAll(".tablinks");
+
+var slideIndex = 1;
+showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  if (n > subjects.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = subjects.length}
+  for (i = 0; i < subjects.length; i++) {
+    subjects[i].style.display = "none";  
   }
-  slides[slideIndex-1].style.display = "block";  
+  subjects[slideIndex-1].style.display = "block";  
+  if(n < subjects.length){
+    subjects[slideIndex].style.display = "block";
+    console.log("in")
+  }
+        
+    console.log(slideIndex-1 , slideIndex)
+    console.log(subjects[slideIndex-1], subjects[slideIndex])
+
 }
 
-prevButton.addEventListener("click", plusSlides(-1))
-nextButton.addEventListener("click", plusSlides(1))
+function displayContent(evt, tabName) {
+    console.log(tabName)
+    for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.querySelector(`#${tabName}`).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
