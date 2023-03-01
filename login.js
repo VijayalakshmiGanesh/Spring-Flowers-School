16ca894d43dc29fbc43b2cb3d31f24a2f61f0887
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-03-01 12:02:37 +0530

    Implemented login functionality

---
+++ login.js
@@ -0,0 +1,17 @@
+const userName = document.querySelector("#username");
+const passWord = document.querySelector("#password");
+const loginBtn = document.querySelector("#submit");
+const errorMsg = document.querySelector("#error-msg");
+
+function loginHandler(){
+    if(userName.value === "admin" && passWord.value === "admin"){
+        window.location.href = "\welcome.html"
+    }else{
+        console.log("yes")
+        errorMsg.classList.remove("d-none");
+        errorMsg.classList.add("d-block")
+    }
+}
+
+
+loginBtn.addEventListener("click", loginHandler)
